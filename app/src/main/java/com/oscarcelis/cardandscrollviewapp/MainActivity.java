44d812c488ff5f754178ac72346d1ce327878520
OscarCelis <oscarcelis6@gmail.com>
2019-04-09 20:07:16 -0500

    Aplicación que muestra el uso de un CardView en un ScrollView tanto vertical como horizontal.

--- app/src/main/java/com/oscarcelis/cardandscrollviewapp/MainActivity.java
+++ app/src/main/java/com/oscarcelis/cardandscrollviewapp/MainActivity.java
@@ -1,8 +1,9 @@
+
 package com.oscarcelis.cardandscrollviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//Desarrollado por Oscar Celis
 public class MainActivity extends AppCompatActivity {
 
     @Override
